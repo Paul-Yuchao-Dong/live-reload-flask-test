0ec023cce36d0fb67f8c7da1b7e1d8a72d9b4ddf
Paul-Yuchao-Dong <Paulcynic@gmail.com>
2017-04-05 17:09:16 +0800

    added live server

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import os
 from flask import Flask, render_template
+from livereload import Server
 
 template_dir = os.path.dirname(__file__)
 template_dir = os.path.join(template_dir, 'templates')
 app = Flask(__name__, template_folder=template_dir)
+
+app.debug = True
+server = Server(app.wsgi_app)
 
 @app.route("/")
 def index():
     return render_template(r'index.html')
 
 if __name__ == "__main__":
-    app.run()
+    server.serve()
